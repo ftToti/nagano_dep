f1aa9cbab5c05e7fdb944d7d1f77eb8f581e7693
ftToti <ft.1rjn@gmail.com>
2020-06-01 02:24:07 +0900

    [Finish]migrate/products

--- db/migrate/20200531094843_create_products.rb
+++ db/migrate/20200531094843_create_products.rb
@@ -1,8 +1,14 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+    	t.integer :product_genre_id
+    	t.string  :name, index: true
+    	t.text    :description
+    	t.string  :image_id
+    	t.integer :unit_price
+    	t.boolean :is_sales, default: true
 
-      t.timestamps
+    	t.timestamps
     end
   end
 end
