e22ee50f897e44925f00c63168e69195223008db
ftToti <ft.1rjn@gmail.com>
2020-06-01 02:02:29 +0900

    [Finish]migrate/product_genres

--- db/migrate/20200531094910_create_product_genres.rb
+++ db/migrate/20200531094910_create_product_genres.rb
@@ -2,6 +2,9 @@ class CreateProductGenres < ActiveRecord::Migration[5.2]
   def change
     create_table :product_genres do |t|
 
+    	t.integer :category, null: false, default: 1
+    	t.boolean :is_enabled, null: false, default: true
+
       t.timestamps
     end
   end
