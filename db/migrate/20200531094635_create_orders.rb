0151918b6b682d262c77aa8822c3cad8bdb5a7af
moriiiku <mo.1221.mrk@gmail.com>
2020-06-01 05:03:54 +0000

    migrate to order and order_products

--- db/migrate/20200531094635_create_orders.rb
+++ db/migrate/20200531094635_create_orders.rb
@@ -2,7 +2,16 @@ class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
 
-      t.timestamps
+
+    t.references :member, foreign_key: true
+    t.integer :postage, null: false, default: 800
+    t.integer :subtotal
+    t.integer :payment_method, null: false, default: 1
+    t.integer :order_status, null: false, default: 1
+    t.string  :postcode
+    t.string  :address
+    t.string  :addressee
+    t.timestamps
     end
   end
 end
