15c10480e716b0729c6469392ce67f0e4d268074
vagrant <vagrant@localhost.localdomain>
2020-06-01 07:17:31 +0000

    member

--- db/schema.rb
+++ db/schema.rb
@@ -40,30 +40,68 @@ ActiveRecord::Schema.define(version: 2020_05_31_095044) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.string "postcode", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "phone_number", default: "", null: false
+    t.boolean "is_enabled", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_members_on_email", unique: true
+    t.index ["first_name"], name: "index_members_on_first_name"
+    t.index ["first_name_kana"], name: "index_members_on_first_name_kana"
+    t.index ["last_name"], name: "index_members_on_last_name"
+    t.index ["last_name_kana"], name: "index_members_on_last_name_kana"
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
 
   create_table "order_products", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "order_id"
+    t.integer "purchase_price"
+    t.integer "number"
+    t.integer "production_status", default: 1, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
+    t.integer "member_id"
+    t.integer "postage", default: 800, null: false
+    t.integer "subtotal"
+    t.integer "payment_method", default: 1, null: false
+    t.integer "order_status", default: 1, null: false
+    t.string "postcode"
+    t.string "address"
+    t.string "addressee"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_orders_on_member_id"
   end
 
   create_table "product_genres", force: :cascade do |t|
+    t.integer "category", default: 1, null: false
+    t.boolean "is_enabled", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
+    t.integer "product_genre_id"
+    t.string "name"
+    t.text "description"
+    t.string "image_id"
+    t.integer "unit_price"
+    t.boolean "is_sales", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_products_on_name"
+    t.index ["product_genre_id"], name: "index_products_on_product_genre_id"
   end
 
   create_table "shipping_addresses", force: :cascade do |t|
