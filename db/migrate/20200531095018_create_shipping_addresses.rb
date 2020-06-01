a551e94755fc8efd8ee1f014a547405d58d70978
vagrant <vagrant@localhost.localdomain>
2020-06-01 04:29:27 +0000

    [migration]member,shipping_address

--- db/migrate/20200531095018_create_shipping_addresses.rb
+++ db/migrate/20200531095018_create_shipping_addresses.rb
@@ -1,7 +1,10 @@
 class CreateShippingAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :shipping_addresses do |t|
-
+    	t.references :member,					null: false, default: "", foreign_key: true
+    	t.string :postcode,						null: false, default: ""
+    	t.string :address,						null: false, default: ""
+    	t.string :addressee,					null: false, default: ""
       t.timestamps
     end
   end
