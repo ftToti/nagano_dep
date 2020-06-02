bfdf715bb54e5a9453bf3511664e302a96722cec
vagrant <vagrant@localhost.localdomain>
2020-06-02 08:46:52 +0000

    member

--- app/controllers/members/shipping_addresses_controller.rb
+++ app/controllers/members/shipping_addresses_controller.rb
@@ -1,2 +1,16 @@
 class Members::ShippingAddressesController < ApplicationController
+	def index
+		@shipping_address = ShippingAddress.new
+		@shipping_addresses = ShippingAddress.all
+	end
+	def create
+		@shipping_address = ShippingAddress.new(shipping_address_params)
+		@shipping_address.save
+		redirect_to shipping_address_path
+	end
+
+	private
+	def shipping_address_params
+		params.require(:shipping_address).permit(:postcode, :address, :addressee)
+	end
 end
