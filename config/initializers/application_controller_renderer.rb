2579425fe88fe339bd9a8f5ca39d66f412a796f7
ftToti <ft.1rjn@gmail.com>
2020-06-03 19:46:56 +0900

    会員側商品ページレイアウト

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '496735ed0ac1036f54bec3359d18cdec74c9af330121da4ddcfffd401123600bb16d126fcf7eb1e0eef46212c2fa17484895907233a5b62021088732a00e897c'
