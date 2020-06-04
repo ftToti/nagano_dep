3dc3c8fc84795e300595ada13e07f00e3b4a37fe
vagrant <vagrant@localhost.localdomain>
2020-06-04 06:09:20 +0000

    member

--- app/controllers/members/members_controller.rb
+++ app/controllers/members/members_controller.rb
@@ -1,2 +1,18 @@
 class Members::MembersController < ApplicationController
+	def show
+		@member = Member.find(params[:id])
+	end
+	def edit
+		@member = Member.find(params[:id])
+	end
+	def update
+		@member = Member.find(params[:id])
+		@member.update(member_params)
+		redirect_to members_member_path(@member)
+	end
+
+	private
+	def member_params
+		params.require(:member).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number)
+	end
 end
