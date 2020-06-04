59f47a60b5208c654a5473203379c23617349b0f
ryu2129 <ryu212920@gmail.com>
2020-06-04 11:33:41 +0000

    add order

--- app/controllers/members/orders_controller.rb
+++ app/controllers/members/orders_controller.rb
@@ -16,7 +16,22 @@ class Members::OrdersController < ApplicationController
     @order =Order.new(order_params)
     @oreder.member_id = current_member.id
     @order.save
-		redirect_to members_orders_confirm_path
+  end
+
+  def confirm
+    @order = Order.new(order_params)
+    @carts = current_member.cart_items
+    @tax = 1.1
+    if params[:shipping_address] == "1"
+      @order.postcode = current_member.postcode
+      @order.address = current_member.address
+      @order.addressee = current_member.last_name + current_member.first_name
+    elsif params[:shipping_address] == "2"
+      @shipping_address = ShippingAddress.find(params[:order][:shipping_address])
+      @order.postcode = @shipping_address.postcode
+      @order.address = @shipping_address.address
+      @order.addressee = @shipping_address.addressee
+    end
   end
 
   def update
@@ -25,9 +40,12 @@ class Members::OrdersController < ApplicationController
 		redirect_to members_orders_thanks_path
   end
 
+  def thanks
+
+  end
 
   private
 	def order_params
-    params.require(:order).permit(:member_id, :postage, :subtotal, :payment_method, :order_status, :postcode, :address, :addressee)
+    params.require(:order).permit(:member_id, :payment_method, :order_status, :postcode, :address, :addressee)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
     delete '/cart_items', to: 'cart_items#destroy_all', as: 'destroy_all'
     get '/disable_confirm', to: 'members#disable_confirm', as: 'disable_confirm'
     patch '/disable/:id', to: 'members#disable', as: 'disable'
-    get '/orders/confirm', to: 'orders#confirm', as: 'confirm'
+    post '/orders/confirm', to: 'orders#confirm', as: 'confirm'
     get '/orders/thanks', to: 'orders#thanks', as: 'thanks'
     get '/products/genre/:id', to: 'products#genre_index', as: 'products_genre'
     resources :members, only: [:show, :edit, :update]
