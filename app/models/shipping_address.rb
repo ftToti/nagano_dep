e2233b025786f2c26a7dd729378c25d85ff0825a
ftToti <ft.1rjn@gmail.com>
2020-06-07 14:34:29 +0900

    注文機能修正

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -4,9 +4,4 @@ class ShippingAddress < ApplicationRecord
   validates :postcode, presence: true
   validates :address, presence: true
   validates :addressee, presence: true
-
-
-  def deli
-  	self.postcode + self.address + self.addressee
-  end
 end
