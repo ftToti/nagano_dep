d3c98be69c751e8a9b1bc25ab7fc571e5bcda945
ftToti <ft.1rjn@gmail.com>
2020-06-09 06:31:58 +0900

    テストの内容に沿って機能修正

--- app/controllers/members/products_controller.rb
+++ app/controllers/members/products_controller.rb
@@ -2,7 +2,7 @@ class Members::ProductsController < ApplicationController
 	def index
 		@tax = 1.1
 		@products = Product.joins(:product_genre).where(product_genres: {is_enabled: true}).page(params[:page]).reverse_order
-		@product = Product.all
+		@pn = Product.joins(:product_genre).where(product_genres: {is_enabled: true}).count
 	end
 
 	def show
@@ -14,6 +14,7 @@ class Members::ProductsController < ApplicationController
 	def genre_index
 		@genre = ProductGenre.find(params[:id])
 		@products = @genre.products.joins(:product_genre).where(product_genres: {is_enabled: true}).page(params[:page]).reverse_order
+		@pn = @genre.products.joins(:product_genre).where(product_genres: {is_enabled: true}).count
 		@tax = 1.1
 	end
 end
